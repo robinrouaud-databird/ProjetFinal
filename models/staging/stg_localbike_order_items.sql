deaed142be99b57d36b4a049eeb9a8dc2854bab8
Robin Rouaud <robinrouaud622@outlook.fr>
2025-10-31 16:22:36 +0000

    Add specific tests

--- models/staging/stg_localbike_order_items.sql
+++ models/staging/stg_localbike_order_items.sql
@@ -4,5 +4,6 @@ select
     product_id AS product_id,
     CAST(quantity AS INTEGER) AS quantity,
     CAST(list_price AS NUMERIC) AS list_price,
-    CAST(discount AS NUMERIC) AS discount
+    CAST(discount AS NUMERIC) AS discount,
+    ROUND(quantity * list_price * (1 - discount), 2) AS order_item_amount
 from {{ source('LocalBike', 'order_items') }}
