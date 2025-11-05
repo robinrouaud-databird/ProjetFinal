0dda4348d30d92a324ed04155505cb0f3ed242a2
Robin Rouaud <robinrouaud622@outlook.fr>
2025-11-05 21:32:50 +0000

    Optimize mart model

--- models/mart/mrt_products_report.sql
+++ models/mart/mrt_products_report.sql
@@ -1,10 +1,10 @@
 SELECT 
     product_name,
     model_year,
-    volume_sales_product,
-    total_revenue_product,
-    total_discount_product,
-    total_revenue_entreprise,
+    coalesce(volume_sales_product, 0) AS volume_sales_product,
+    ROUND(coalesce(total_revenue_product, 0), 2) AS total_revenue_product,
+    coalesce(total_discount_product, 0) AS total_discount_product,
+    ROUND(total_revenue_entreprise, 2) AS total_revenue_entreprise,
     volume_sales_entreprise,
     stock_product, 
     CAST(average_stock AS INTEGER) as average_stock
