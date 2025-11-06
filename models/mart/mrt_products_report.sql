99b8cdc24f6c77787be9be4f8821578521ee7bd9
Robin Rouaud <robinrouaud622@outlook.fr>
2025-11-06 19:22:09 +0000

    Change staging column types

--- models/mart/mrt_products_report.sql
+++ models/mart/mrt_products_report.sql
@@ -2,12 +2,12 @@ SELECT
     product_name,
     model_year,
     coalesce(volume_sales_product, 0) AS volume_sales_product,
-    ROUND(coalesce(total_revenue_product, 0), 2) AS total_revenue_product,
+    coalesce(total_revenue_product, 0) AS total_revenue_product,
     coalesce(total_discount_product, 0) AS total_discount_product,
-    ROUND(total_revenue_entreprise, 2) AS total_revenue_entreprise,
+    total_revenue_entreprise AS total_revenue_entreprise,
     volume_sales_entreprise,
     stock_product, 
     CAST(average_stock AS INTEGER) as average_stock
-FROM {{ ref('int_localbike_stocks') }}
-left join {{ ref('int_localbike_products') }}
+FROM {{ ref('int_localbike_products') }} 
+left join {{ ref('int_localbike_stocks') }}
  USING (product_id)
